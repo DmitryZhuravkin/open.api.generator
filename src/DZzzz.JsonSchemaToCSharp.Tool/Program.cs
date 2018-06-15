ba4c6556ede78fcac26e8c02dc2587784fa26159
Dmitry.Zhuravkin <demion_07@list.ru>
2018-06-15 19:52:30 +0300

    JSon model generation for swagger

---
+++ src/DZzzz.JsonSchemaToCSharp.Tool/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace DZzzz.JsonSchemaToCSharp.Tool
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            JsonSchema4 schema = JsonSchema4.FromFileAsync("apiDeclaration.json").Result;
+
+            CSharpGenerator generator = new CSharpGenerator(schema, new CSharpGeneratorSettings());
+            var file = generator.GenerateFile();
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ src/DZzzz.Swag.Generator.Core/Configuration/GeneratorSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace DZzzz.Swag.Generator.Core.Configuration
+{
+    public class GeneratorSettings
+    {
+        //[JsonProperty("swaggerURL")]
+        public string SwaggerUrl { get; set; }
+
+        //[JsonProperty("swaggerFormat")]
+        public string SwaggerFormat { get; set; }
+
+        //[JsonProperty("swaggerVersion")]
+        public string SwaggerVersion { get; set; }
+
+        //[JsonProperty("outputFolder")]
+        public string OutputFolder { get; set; }
+
+        //[JsonProperty("outputSolutionName")]
+        public string OutputSolutionName { get; set; }
+
+        //[JsonProperty("language")]
+        public string Language { get; set; }
+    }
+}
